d86b91468e3c519f9988fcdc7590310d369fdaac
Heberty <hebertyprado22005@gmail.com>
2022-06-04 16:26:58 -0300

    create 6 class

---
+++ Aula6/app-menu/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Header from './Header'
+import Home from './Home'
+import Produtos from './Produtos'
+
+const App = () => {
+
+    const { pathname } = window.location;
+
+    let Pagina = Home;
+
+    if(pathname === "/produtos")
+        Pagina = Produtos;
+
+  return (
+    <section>
+        <Header />
+        <Pagina />
+    </section>
+  )
+}
+
+export default App
